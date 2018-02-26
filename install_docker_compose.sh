ab5b069220f6e115b40a18cc548912d01a2daa9f
Ein Verne <einverne@gmail.com>
2018-02-26 20:44:38 +0800

    Add docker compose install script
    Signed-off-by: Ein Verne <einverne@gmail.com>

---
+++ install_docker_compose.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -
+
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
