d4b62c069bbde12ea13a0e0eff1f9334793a6a8e
Ein Verne <i@einverne.info>
2022-03-02 10:12:14 +0800

    Update docker-compose version

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
